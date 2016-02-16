468085fd0e94ec4eeb5c0739a5fba8a8f4e0f516
N00bface <vmjari@hotmail.com>
2016-02-16 11:17:32 +0100

    0.0.1

---
+++ the_Company/src/main/java/Game.java
@@ -0,0 +1,10 @@
+import com.googlecode.lanterna.terminal.Terminal;
+
+/**
+ * @autor Jari Van Melckebeke
+ */
+public class Game {
+    public Game(Terminal mainTerminal) {
+
+    }
+}
---
+++ the_Company/src/main/java/LoadScreen.java
@@ -0,0 +1,10 @@
+import com.googlecode.lanterna.terminal.Terminal;
+
+/**
+ * @autor Jari Van Melckebeke
+ */
+public class LoadScreen {
+    public LoadScreen(Terminal mainTerminal) {
+
+    }
+}
