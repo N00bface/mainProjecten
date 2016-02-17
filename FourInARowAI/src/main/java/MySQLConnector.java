8813532b8d8cad254eb44aee48a3cc2c60152022
Jari Van M <vmjari@hotmail.com>
2016-02-17 21:31:01 +0100

    0.0.3

---
+++ FourInARowAI/src/main/java/MySQLConnector.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+/**
+ * Created by jariv on 17/02/2016.
+ */
+public class MySQLConnector {
+    private ResultSet set;
+
+    public void getSet() {
+        Connection connection = DriverManager.getConnection()
+    }
+}
