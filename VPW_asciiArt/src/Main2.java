fc98f581e9570acd6691a1d83ca5023451a5d24e
Jari Van M <vmjari@hotmail.com>
2016-03-01 13:27:58 +0100

    0.1 input

--- VPW_asciiArt/src/Main2.java
+++ VPW_asciiArt/src/Main2.java
@@ -1,4 +1,21 @@
 
 public static void main(String[] args){
-
+  Scanner sc = new Scanner(System.in);
+  int aantalTestGevallen = sc.nextInt();
+  for(int i = 0; i< aantalTestGevallen; i++){
+    int width = sc.nextInt();
+    int height = sc.nextInt();
+    String[] ascii = new String[height];
+    for(int j = 0; j< height; j++){
+      ascii[j] = sc.nextLine();
+    }
+    int aantalStukken = sc.nextInt();
+    for(int j = 0;j< aantalStukken;j++){
+      int width_stuk = sc.nextInt();
+      int height_stuk = sc.nextInt();
+      for(int h = 0; h< width_stuk; h++){
+        
+      }
+    }
+  }
 }
