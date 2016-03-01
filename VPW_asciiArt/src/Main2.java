389f13a2941f2f7490ec94708e374fd598842f33
Jari Van M <vmjari@hotmail.com>
2016-03-01 13:50:11 +0100

    Update Main2.java

--- VPW_asciiArt/src/Main2.java
+++ VPW_asciiArt/src/Main2.java
@@ -9,13 +9,28 @@ public static void main(String[] args){
     for(int j = 0; j< height; j++){
       ascii[j] = sc.nextLine();
     }
-    int aantalStukken = sc.nextInt();
+    ArrayList<String[]> stukken = new ArrayList<String[]>();
     for(int j = 0;j< aantalStukken;j++){
       int width_stuk = sc.nextInt();
       int height_stuk = sc.nextInt();
-      for(int h = 0; h< width_stuk; h++){
-        
+      String[] ascii_stuk = new String[height_stuk];
+      for(int h = 0; h< height_stuk; h++){
+        ascii_stuk[h] = sc.nextLine();
       }
+      System.out.println(getAnswer(width, height,width_stuk, height_stuk, ascii, stuk));
     }
+    
+  }
+}
+
+/**
+ * deze methode verkrijgt de mogenlijkheid om een ascii_stuk in de ascii afbeelding te steken 
+ */
+private static String getAnswer(int width, int height,int stuk_height, int stuk_width, String[] ascii, String[] stuk){
+  //1: bereken de combo's
+  char[][][] stuk_chars = new char[4][stuk_height][stuk_width];
+  //1.1: steek oorspronkelijke chars in de stukken
+  for(String lijn: stuk){
+    
   }
 }
