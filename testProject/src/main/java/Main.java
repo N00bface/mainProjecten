bcb5a69069ea5a500c2cef3fd0314d010e2d4f29
N00bface <vmjari@hotmail.com>
2016-04-13 22:22:02 +0200

    moved VPW to competitive programming repo

--- testProject/src/main/java/Main.java
+++ testProject/src/main/java/Main.java
@@ -1,4 +1,11 @@
-import java.sql.*;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
 
 /**
  * @autor Jari Van Melckebeke
@@ -7,21 +14,39 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "tanzania");
         Statement statement = con.createStatement();
-        statement.execute("use parameterDB");
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                for (int k = 0; k < 10; k++) {
-                    for (int l = 0; l < 10; l++) {
-                        for (int m = 0; m < 10; m++) {
-                            String query = "insert into parameters (parameter_isWinPossible,parameter_isOtherWinPossible,parameter_is3Possible,parmeter_isOther3Possible,parameter_is2OptionsPossible)" +
-                                    "values (" + i + "," + j + "," + k + "," + l + "," + m + ")";
-                            PreparedStatement preparedStatement = con.prepareStatement(query);
-                            preparedStatement.execute();
-                            System.out.println(i+" "+j+" "+k+" "+l+" "+m);
-                        }
+        statement.execute("use teresaDB");
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(new File("/home/jari/Downloads/GeoLiteCity-Location.csv"));
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = null;
+            bufferedReader.readLine();
+            bufferedReader.readLine();
+            while ((line = bufferedReader.readLine()) != null) {
+                line = line.replaceAll("\"", "");
+                line = line.replaceAll("\'", "\\'");
+                line = line.trim();
+                String[] data = line.split(",");
+                for (int i = 0; i < data.length; i++) {
+                    if (data[i].equals("")) {
+                        data[i] = "null";
+                    }
+                    data[i] = "\'" + data[i] + "\'";
+                }
+                System.out.println(Arrays.toString(data));
+                if (data[1].contains("BE")) {
+                    try {
+                        statement.execute("insert into city (locId,city,latitude,longitude) values (" + data[0] + "," + data[3] + "," + data[5] + "," + data[6] + ");");
+                    } catch (MySQLSyntaxErrorException ignored) {
                     }
                 }
             }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+
     }
 }
