ee0e912b669d012620f0c98f0b5b66562f1ae254
N00bface <vmjari@hotmail.com>
2016-05-10 16:56:41 +0200

    NEW PROJECT

--- testProject/src/main/java/Main.java
+++ testProject/src/main/java/Main.java
@@ -1,52 +1,19 @@
-import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
-
-import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Arrays;
+import java.util.Random;
 
 /**
- * @autor Jari Van Melckebeke
+ * @author Jari Van Melckebeke
  */
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "tanzania");
-        Statement statement = con.createStatement();
-        statement.execute("use teresaDB");
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(new File("/home/jari/Downloads/GeoLiteCity-Location.csv"));
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = null;
-            bufferedReader.readLine();
-            bufferedReader.readLine();
-            while ((line = bufferedReader.readLine()) != null) {
-                line = line.replaceAll("\"", "");
-                line = line.replaceAll("\'", "\\'");
-                line = line.trim();
-                String[] data = line.split(",");
-                for (int i = 0; i < data.length; i++) {
-                    if (data[i].equals("")) {
-                        data[i] = "null";
-                    }
-                    data[i] = "\'" + data[i] + "\'";
-                }
-                System.out.println(Arrays.toString(data));
-                if (data[1].contains("BE")) {
-                    try {
-                        statement.execute("insert into city (locId,city,latitude,longitude) values (" + data[0] + "," + data[3] + "," + data[5] + "," + data[6] + ");");
-                    } catch (MySQLSyntaxErrorException ignored) {
-                    }
-                }
+    public static void main(String[] args) {
+        System.out.println("100");
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            String out = "";
+            for (int j = 0; j < 100; j++) {
+                out += random.nextInt(999999) + " ";
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            out += random.nextInt(999999);
+            System.out.println(out);
         }
-
-
     }
 }
