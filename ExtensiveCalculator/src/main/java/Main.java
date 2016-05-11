a553b23d9c8aceb922f12b10dbe04b18ad63f962
N00bface <vmjari@hotmail.com>
2016-05-11 20:53:02 +0200

    Second commit

---
+++ ExtensiveCalculator/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import Gui.MainGui;
+import OtherIO.MainResources;
+
+/**
+ * @author Jari Van Melckebeke
+ * @version 1
+ * @since 8 May 2016
+ * this class represents the whole runtime
+ */
+public class Main {
+    /**
+     * Main method
+     *
+     * @param args default System args
+     */
+    public static void main(String[] args) {
+        MainResources main = new MainResources();
+    }
+}
---
+++ ExtensiveCalculator/src/main/java/OtherIO/MainResources.java
@@ -0,0 +1,13 @@
+package OtherIO;
+
+import Gui.MainGui;
+
+/**
+ * @author Jari Van Melckebeke
+ */
+public class MainResources {
+    public static Gui.MainGui mainGui;
+    public MainResources(){
+        mainGui = new MainGui();
+    }
+}
